c42cedcf72cf05922ee51e9149a3dfc715926327
Erik <erik_grip@hotmail.com>
2020-07-12 09:32:03 +0200

    Make memory class can_provide method take min memory size to train as input, instead of knowing min size or batch size.

--- agent/memory/replay.py
+++ agent/memory/replay.py
@@ -3,9 +3,8 @@ import random
 
 # Create class to store the Experience objects
 class StandardReplayMemory():
-    def __init__(self, capacity, min_size_for_sampling):
+    def __init__(self, capacity):
         self.memory = deque(maxlen=capacity)
-        self.min_size = min_size_for_sampling
         
     def push(self, experience):
             self.memory.append(experience)
@@ -13,7 +12,10 @@ class StandardReplayMemory():
     def sample(self, batch_size):
         return random.sample(self.memory, batch_size)
     
-    def can_provide(self):
-        return len(self.memory) >= self.min_size
+    def can_provide(self, min_size_to_train):
+        return len(self.memory) >= min_size_to_train
+    
+    def size(self):
+        return len(self.memory)
 
 
