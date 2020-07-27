447ea97cda19dd11d126b045f2814316c54700f1
Erik <erik_grip@hotmail.com>
2020-07-27 11:00:33 +0200

    Add functinonality for parameter optimization through searching a set number of combinations of parameter choices from spec. Added utility functions for converting from nested spec to flat pandas df and back again.

--- Train.py
+++ Train.py
@@ -2,7 +2,8 @@ from env.game_manager import TicTacToeGameManager
 from agent.agent import Agent
 from agent.algorithm.dqn import DQN
 from util.utils import (
-    sequential_model_from_spec, strategy_from_spec, memory_from_spec)
+    sequential_model_from_spec, strategy_from_spec, memory_from_spec,
+    spec_search_combinations)
 from util.tensorboard_mod import ModifiedTensorBoard
 import os
 import time
@@ -33,18 +34,24 @@ default_spec = {
             },
             {'class_name': 'Dense',
              'config': 
-                 {'units': 64,
+                 {'units': [8, 16, 32, 64, 128],
+	              'activation': 'relu'
+                  }
+            },
+            {'class_name': 'Dense',
+             'config': 
+                 {'units': [8, 16, 32, 64, 128],
                   'activation': 'relu'
                   }
             },
             {'class_name': 'Dense',
              'config': 
-                 {'units': 4,
+                 {'units': 9,
                   'activation': 'softmax'
                   }
             }
         ],
-        'lr':  0.02,
+        'lr':  [0.05, 0.01, 0.005, 0.001],
         'loss': 'mse',
         'keras_version': '2.3.1'
     },
@@ -54,14 +61,17 @@ default_spec = {
     },
     'algorithm': {
         'type': 'DQN',
-        'target_net_update_freq': 500,
+        'target_net_update_freq': 2_500,
         'discount': 0.99
     },
     'train': {
-        'num_episodes': 100,
-        'minibatch_size': 64,
-        'min_memory': 64
-    }
+        'num_episodes': 10_000,
+        'minibatch_size': 128,
+        'min_memory': 128
+    },
+    'search': {
+        'max_combinations': 10
+        }
 }
     
 
@@ -74,142 +84,146 @@ OPPONENT_MODEL = False
 #  Model save and stats settings
 AGGREGATE_STATS_EVERY = 50  # episodes
 
-def main(spec):
+def main(input_spec):
+    spec_list = spec_search_combinations(input_spec)
     
-    MODE = spec['environment']['mode']
-    MODEL_NAME = spec['net']['name']
-    EPISODES = spec['train']['num_episodes']
-    MIN_MEMORY_TO_TRAIN = spec['train']['min_memory']
-    MINIBATCH_SIZE = spec['train']['minibatch_size']
-    DISCOUNT = spec['algorithm']['discount']
-    UPDATE_TARGET_EVERY = spec['algorithm']['target_net_update_freq']
+    # Loop over sample of parameter combinations from input spec
+    for spec in spec_list:
     
-    # Create output folders
-    if not os.path.isdir('models'):
-        os.makedirs('models')
-    if not os.path.isdir('replay_history'):
-        os.makedirs('replay_history')
-    if not os.path.isdir('specs'):
-        os.makedirs('specs')
+        MODE = spec['environment']['mode']
+        MODEL_NAME = spec['net']['name']
+        EPISODES = spec['train']['num_episodes']
+        MIN_MEMORY_TO_TRAIN = spec['train']['min_memory']
+        MINIBATCH_SIZE = spec['train']['minibatch_size']
+        DISCOUNT = spec['algorithm']['discount']
+        UPDATE_TARGET_EVERY = spec['algorithm']['target_net_update_freq']
         
-    tensorboard = ModifiedTensorBoard(
-        log_dir=f"logs/{MODEL_NAME}-{int(time.time())}") 
-    
-    # We'll use experiences from replay memory to train the network.Include next 
-    # valid action because else model won't know which future qvalues to discard
-    Experience = namedtuple(
-        'Experience',
-        ('state', 'action', 'reward', 
-         'next_state', 'next_valid_actions','is_terminal_state'))
-                    
-    
-    env = TicTacToeGameManager(mode=MODE)
-    strategy = strategy_from_spec(spec['strategy'])
-    memory = memory_from_spec(spec['replay_memory'])      
-    model = DQN(default_spec['net'])
-    agent = Agent(strategy, model)
-    
-    '''             
-    if MODE == 'human':
-        # Load saved games into replay memory
-        saved_states = shelve.open("replay_history/replay_history") 
-        for key in saved_states.keys():
-            try:
-                for experience in saved_states[key]:
-                    agent.model.memory.push(experience)
-            except EOFError:
-                pass 
-        saved_states.close()
-    '''
+        # Create output folders
+        if not os.path.isdir('models'):
+            os.makedirs('models')
+        if not os.path.isdir('replay_history'):
+            os.makedirs('replay_history')
+        if not os.path.isdir('specs'):
+            os.makedirs('specs')
+            
+        tensorboard = ModifiedTensorBoard(
+            log_dir=f"logs/{MODEL_NAME}-{int(time.time())}") 
         
-    # Loop over episodes
-    saved_games = []
-    ep_rewards = [] 
-    for episode in tqdm(range(1, EPISODES+1), ascii=True, unit='episode'):
-        tensorboard.step = episode
-        episode_reward = 0
+        # Include next valid action because else model won't know 
+        # which future qvalues to discard
+        Experience = namedtuple(
+            'Experience',
+            ('state', 'action', 'reward', 
+             'next_state', 'next_valid_actions','is_terminal_state'))
+                        
         
-        current_state = env.reset()
+        env = TicTacToeGameManager(mode=MODE)
+        strategy = strategy_from_spec(spec['strategy'])
+        memory = memory_from_spec(spec['replay_memory'])      
+        model = DQN(spec['net'])
+        agent = Agent(strategy, model)
         
-        done = False
-        while not done:
-            # Select and perform action
-            action = agent.get_action(current_state, env.valid_actions())
-            next_state, reward, done = env.step(action)
-            
-            # Make opponent move
-            if not done:
-                env_action = env.get_env_action(next_state)
-                next_state, reward, done = env.step(env_action)
-                reward = -reward  # Assuming symmetric rewards
-                
-            next_valid_actions = env.valid_actions()
-            
-            # Store experience in replay memory
-            memory.push(Experience(current_state/255, action, reward, 
-                                   next_state/255, next_valid_actions, done))
-            
-            
-            # Train model
-            if memory.can_provide(MIN_MEMORY_TO_TRAIN):
-                minibatch = memory.sample(MINIBATCH_SIZE)
-                model.train(minibatch,
-                            discount=DISCOUNT, 
-                            game_done=done,
-                            callbacks=[tensorboard])
-            
-            # Get epsilon and update rewards and state
-            episode_reward += reward
-            current_state = next_state
-            explore_param = agent.strategy.get_decayed_rate() 
-            
-        # Append episode reward to a list and log stats
-        ep_rewards.append(episode_reward)
-        
-        # Keep game if played against human
+        '''             
         if MODE == 'human':
-            saved_games.append(env.game_history)
+            # Load saved games into replay memory
+            saved_states = shelve.open("replay_history/replay_history") 
+            for key in saved_states.keys():
+                try:
+                    for experience in saved_states[key]:
+                        agent.model.memory.push(experience)
+                except EOFError:
+                    pass 
+            saved_states.close()
+        '''
             
-        # Update target net to equal policy net
-        if episode % UPDATE_TARGET_EVERY == 0:
-            model.update_target_weights()
+        # Loop over episodes
+        saved_games = []
+        ep_rewards = [] 
+        for episode in tqdm(range(1, EPISODES+1), ascii=True, unit='episode'):
+            tensorboard.step = episode
+            episode_reward = 0
             
-        # Update tensorboard and save model
-        if not episode % AGGREGATE_STATS_EVERY or episode == 1:
-            latest_rews = ep_rewards[-AGGREGATE_STATS_EVERY:]
-            average_reward = sum(latest_rews) / len(latest_rews)
-            min_reward = min(latest_rews)
-            max_reward = max(latest_rews)
-            pct_win = latest_rews.count(env.win_reward()) / len(latest_rews)
-            pct_draw = latest_rews.count(env.draw_reward()) / len(latest_rews)
-            pct_loss = latest_rews.count(env.loss_penalty()) / len(latest_rews)
-            tensorboard.update_stats(reward_avg=average_reward, 
-                                     reward_min=min_reward, 
-                                     reward_max=max_reward, 
-                                     win_percent=pct_win,
-                                     draw_percent=pct_draw,
-                                     loss_percent=pct_loss,
-                                     exploration_parameter=explore_param)
-    
-    # Save model
-    model_file_name = f"models/{MODEL_NAME}_{average_reward:_>7.2f}avg_" \
-        + f"{min_reward:_>7.2f}min_{int(time.time())}.model"
-    model.policy_model.model.save(model_file_name)
-    
-    # Save spec with name matching models
-    spec_file_name = f"specs/{MODEL_NAME}_{int(time.time())}_spec.json"
-    with open(spec_file_name, 'w') as json_file:
-        json.dump(spec, json_file)
-    
-    # Keep games if player manually
-    if MODE == 'human':
-        # Save replay memory to file
-        replay_history = shelve.open("replay_history/replay_history")
-        games_db = shelve.open("replay_history/saved_games") 
-        replay_history[f"{MODEL_NAME}_{int(time.time())}"] = memory.memory
-        games_db[f"{MODEL_NAME}_{int(time.time())}"] = saved_games
-        replay_history.close()
-        games_db.close()
+            current_state = env.reset()
+            
+            done = False
+            while not done:
+                # Select and perform action
+                action = agent.get_action(current_state, env.valid_actions())
+                next_state, reward, done = env.step(action)
+                
+                # Make opponent move
+                if not done:
+                    env_action = env.get_env_action(next_state)
+                    next_state, reward, done = env.step(env_action)
+                    reward = -reward  # Assuming symmetric rewards
+                    
+                next_valid_actions = env.valid_actions()
+                
+                # Store experience in replay memory
+                memory.push(
+                    Experience(current_state/255, action, reward, 
+                               next_state/255, next_valid_actions, done))
+                
+                # Train model
+                if memory.can_provide(MIN_MEMORY_TO_TRAIN):
+                    minibatch = memory.sample(MINIBATCH_SIZE)
+                    model.train(minibatch,
+                                discount=DISCOUNT, 
+                                game_done=done,
+                                callbacks=[tensorboard])
+                
+                # Get epsilon and update rewards and state
+                episode_reward += reward
+                current_state = next_state
+                explore_param = agent.strategy.get_decayed_rate() 
+                
+            # Append episode reward to a list and log stats
+            ep_rewards.append(episode_reward)
+            
+            # Keep game if played against human
+            if MODE == 'human':
+                saved_games.append(env.game_history)
+                
+            # Update target net to equal policy net
+            if episode % UPDATE_TARGET_EVERY == 0:
+                model.update_target_weights()
+                
+            # Update tensorboard and save model
+            if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+                new_rews = ep_rewards[-AGGREGATE_STATS_EVERY:]
+                average_reward = sum(new_rews) / len(new_rews)
+                min_reward = min(new_rews)
+                max_reward = max(new_rews)
+                pct_win = new_rews.count(env.win_reward()) / len(new_rews)
+                pct_draw = new_rews.count(env.draw_reward()) / len(new_rews)
+                pct_loss = new_rews.count(env.loss_penalty()) / len(new_rews)
+                tensorboard.update_stats(reward_avg=average_reward, 
+                                         reward_min=min_reward, 
+                                         reward_max=max_reward, 
+                                         win_percent=pct_win,
+                                         draw_percent=pct_draw,
+                                         loss_percent=pct_loss,
+                                         exploration_parameter=explore_param)
+        
+        # Save model
+        model_file_name = f"models/{MODEL_NAME}_{average_reward:_>7.2f}avg_" \
+            + f"{min_reward:_>7.2f}min_{int(time.time())}.model"
+        model.policy_model.model.save(model_file_name)
+        
+        # Save spec with name matching models
+        spec_file_name = f"specs/{MODEL_NAME}_{int(time.time())}_spec.json"
+        with open(spec_file_name, 'w') as json_file:
+            json.dump(spec, json_file)
+        
+        # Keep games if player manually
+        if MODE == 'human':
+            # Save replay memory to file
+            replay_history = shelve.open("replay_history/replay_history")
+            games_db = shelve.open("replay_history/saved_games") 
+            replay_history[f"{MODEL_NAME}_{int(time.time())}"] = memory.memory
+            games_db[f"{MODEL_NAME}_{int(time.time())}"] = saved_games
+            replay_history.close()
+            games_db.close()
         
 
 if __name__ == '__main__':
--- util/utils.py
+++ util/utils.py
@@ -1,4 +1,6 @@
 import json
+import pandas as pd
+import numpy as np
 from keras.models import  Sequential, model_from_json
 from keras.optimizers import Adam
 from keras.layers import Activation, Conv2D, Dense, Flatten, Dropout
@@ -7,6 +9,7 @@ from tic_tac_toe.agent.strategy.boltzmann import Boltzmann
 from tic_tac_toe.agent.strategy.max_strategy import MaxStrategy
 from tic_tac_toe.agent.memory.replay import StandardReplayMemory
 from keras.models import load_model
+
 
 def sequential_model_from_spec(net_spec):
 
@@ -62,3 +65,80 @@ def memory_from_spec(memory_spec):
     else:
         raise ValueError("Invalid memory specification")
     return memory
+
+
+def _spec_to_df(spec):
+    # Help function for flattening nested dict
+    def flatten(input_dict, sep='.', prefix=''):
+        out_dict = {}
+        for k, v in input_dict.items():
+            if isinstance(v, dict) and v:
+                deeper = flatten(v, sep, prefix+k+sep)
+                out_dict.update({k2: v2 for k2, v2 in deeper.items()})
+            elif isinstance(v, list) and v:
+                for i, l in enumerate(v, start=1):
+                    if isinstance(l, dict) and l:
+                        deeper = flatten(l, sep, prefix+k+sep+str(i)+sep)
+                        out_dict.update({k2: v2 for k2, v2 in deeper.items()})
+                    else:
+                        out_dict[prefix+k] = v
+            else:
+                out_dict[prefix+k] = v
+        return out_dict
+    
+    d = flatten(spec)
+    df = pd.DataFrame.from_dict(d, orient='index').transpose()
+    return df
+
+def _df_to_spec(df):
+    # Help function for recursively nesting flat dict by key separator
+    def _nest_dict_rec(k, v, out, separator):
+        k, *rest = k.split(separator, 1)
+        if rest:
+            _nest_dict_rec(rest[0], v, out.setdefault(k, {}), separator)
+        else:
+            out[k] = v
+    
+    def nest_dict(flat, separator='.'):
+        result = {}
+        for k, v in flat.items():
+            _nest_dict_rec(k, v, result, separator)
+        return result
+    
+    d = df.transpose().to_dict()[0]
+    d = nest_dict(d)
+    # Remove layer enumaration addded in spec_to_df
+    if 'layers' in d['net'].keys():
+        d['net']['layers'] = [v for k, v in d['net']['layers'].items()]
+    return d
+
+
+def spec_search_combinations(spec_in):
+    ''' Takes a spec file (possibly) with iterable parameter values over which
+        to search. Returns a list of spec dictionaries with only scalar values.
+        The output list holds maximum number of specs determined by 
+        spec['search']['max_combinations']. Returned specs are sampled from 
+        all possible combinations '''
+    specs_out = []
+    df = _spec_to_df(spec_in)
+
+    # Get all possible combinations of parameters from spec
+    for col in df.columns:
+        if not col == 'net.layers.1.config.input_shape':  # Leave input shape
+            df = df.explode(col)
+
+    if len(df) == 1:
+        return [spec_in]  # Return input spec if no values to search over
+    else:
+        try:
+            max_num_specs_out = spec_in['search']['max_combinations']
+        except:
+            raise ValueError('Specify number of parameter combinations to search')
+            
+        sample = df.sample(np.min([max_num_specs_out, len(df)]))
+        for index, row in sample.iterrows():
+            spec = _df_to_spec(pd.DataFrame(data=[row]))
+            specs_out.append(spec)
+    return specs_out
+    
+    
