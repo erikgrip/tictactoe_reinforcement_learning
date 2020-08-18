6c106acc51668a7c5fe81f828670cc8f477e0f0b
dasayev <jupyter@tensorflow-2-2-20200730-154015.europe-west1-b.c.deeprl-284912.internal>
2020-08-18 20:31:50 +0000

    Update modified tensorboard to (hopefully!) work with TF 2.2 i Google Cloudn

--- tic_tac_toe/util/tensorboard_mod.py
+++ tic_tac_toe/util/tensorboard_mod.py
@@ -4,12 +4,14 @@ from keras.callbacks import TensorBoard
 # Own Tensorboard class - to override behaviour of writing a log file for
 # every fit. We'd get more log files than what would be practical.
 # Access with tensorboard --logdir=../../proj/tic_tac_toe/logs/ --host localhost --port 8088
-class ModifiedTensorBoard(TensorBoard): 
+class ModifiedTensorBoard(TensorBoard):
+
     # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.step = 1
-        self.writer = tf.summary.FileWriter(self.log_dir)
+        self.writer = tf.summary.create_file_writer(self.log_dir)
+        self._log_write_dir = self.log_dir
 
     # Overriding this method to stop creating default log writer
     def set_model(self, model):
@@ -29,7 +31,17 @@ class ModifiedTensorBoard(TensorBoard):
     def on_train_end(self, _):
         pass
 
+    def on_train_batch_end(self, batch, logs=None):
+        pass
+
     # Custom method for saving own metrics
     # Creates writer, writes custom metrics and closes writer
     def update_stats(self, **stats):
         self._write_logs(stats, self.step)
+
+    def _write_logs(self, logs, index):
+        with self.writer.as_default():
+            for name, value in logs.items():
+                tf.summary.scalar(name, value, step=index)
+                self.step += 1
+                self.writer.flush()
