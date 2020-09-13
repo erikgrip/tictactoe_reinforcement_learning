d9781eb93e0628f027a7bb912f9ba87a0b677c4f
Erik <erik_grip@hotmail.com>
2020-09-13 22:03:22 +0200

    Cosmetic changes to replay.py

--- tic_tac_toe/agent/memory/replay.py
+++ tic_tac_toe/agent/memory/replay.py
@@ -1,18 +1,16 @@
 from collections import deque
-import random 
+import random
 
 # Create class to store the Experience objects
 class StandardReplayMemory():
     def __init__(self, capacity):
         self.memory = deque(maxlen=capacity)
-        
+
     def push(self, experience):
             self.memory.append(experience)
-        
-    def sample(self, batch_size):
+
+    def sample(self, batch_size) -> list:
         return random.sample(self.memory, batch_size)
-    
+
     def can_provide(self, size):
         return len(self.memory) >= size
-
-
