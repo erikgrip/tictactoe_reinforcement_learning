6bc8a692d078dd3be66b9cb0e8f9bd42c2f79a22
Erik <erik_grip@hotmail.com>
2020-09-20 16:30:00 +0200

    Add try-except block to raise ValueError when select_action fails. Also add logic not to generate random number when threshold is 0.

--- tic_tac_toe/agent/strategy/epsilon_greedy.py
+++ tic_tac_toe/agent/strategy/epsilon_greedy.py
@@ -1,17 +1,20 @@
-from tic_tac_toe.agent.strategy.base import BaseStratregy
+from tic_tac_toe.agent.strategy.base_strategy import BaseStrategy
 import numpy as np
 
-class EpsilonGreedyStrategy(BaseStratregy):
+class EpsilonGreedyStrategy(BaseStrategy):
     def __init__(self, start, end, decay):
         super().__init__(start, end, decay)
 
     def select_action(self, aq_pairs):
         threshold = self.get_decayed_rate()
-        if np.random.random() > threshold:
-            # Get action corresponding to maximum q
-            action = aq_pairs[np.argmax(aq_pairs[:, 1])][0]
-        else: 
-            action = np.random.choice(aq_pairs[:, 0])
-        
+        try:
+            if (threshold == 0) or (np.random.random() > threshold):
+                # Get action corresponding to maximum q
+                action = aq_pairs[np.argmax(aq_pairs[:, 1])][0]
+            else:
+                action = np.random.choice(aq_pairs[:, 0])
+        except:
+            raise ValueError(f"{aq_pairs} not valid action/q-value array")
+
         self.current_step += 1
         return int(action)
