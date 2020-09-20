5e880b3a6b02811de490552635ff927a3c57a2db
Erik <erik_grip@hotmail.com>
2020-09-20 16:10:00 +0200

    Add try-except to raise ValueError when failing to get max Q from input. Also fix format and typos.

--- tic_tac_toe/agent/strategy/boltzmann.py
+++ tic_tac_toe/agent/strategy/boltzmann.py
@@ -1,26 +1,27 @@
-from tic_tac_toe.agent.strategy.base import BaseStratregy
+from tic_tac_toe.agent.strategy.base_strategy import BaseStrategy
 import numpy as np
 
-class Boltzmann(BaseStratregy):
+class Boltzmann(BaseStrategy):
     def __init__(self, start, end, decay):
         super().__init__(start, end, decay)
-         
-    def action_probs(self, aq_pairs):
-        # As defined by Graessner anf Keng p.86. 
+
+    def _action_probs(self, aq_pairs):
+        # As defined by Graessner anf Keng p.86.
         tau = self.get_decayed_rate()
         tau = np.max([tau, 0.001])  # Tau=0 will lead to division by zero
-        qs = np.array(aq_pairs[:, 1])
-        # Normalize to avoid overflow. The output probability is 
-        # insensible to shifts in values of qs
-        qs = qs - qs.max()
+        try:
+            qs = np.array(aq_pairs[:, 1])
+            # Normalize to avoid overflow. The output probability is
+            # insensitive to shifts in values of qs
+            qs = qs - qs.max()
+        except:
+            raise ValueError(f"Unable to find action for max Q in {aq_pairs}")
         ps = (np.exp(qs/tau)) / (np.exp(qs/tau).sum())
         return ps
-    
+
     def select_action(self, aq_pairs):
-        ps = self.action_probs(aq_pairs)
+        ps = self._action_probs(aq_pairs)
         sampled_index = np.argmax(np.random.multinomial(n=1, pvals=ps))
         action = int(aq_pairs[sampled_index][0])
         self.current_step += 1
         return action
-
-
