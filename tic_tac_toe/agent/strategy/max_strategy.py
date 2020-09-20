2ffebffafd2fae0acde466f2ab4a0f68ef142090
Erik <erik_grip@hotmail.com>
2020-09-20 16:53:53 +0200

    Remove option to specify arguments when MaxStrategy is created

--- tic_tac_toe/agent/strategy/max_strategy.py
+++ tic_tac_toe/agent/strategy/max_strategy.py
@@ -1,7 +1,5 @@
 from tic_tac_toe.agent.strategy.epsilon_greedy import EpsilonGreedyStrategy
 
 class MaxStrategy(EpsilonGreedyStrategy):
-    def __init__(self, start=0, end=0, decay=0):
-        super().__init__(start, end, decay)
-
-
+    def __init__(self):
+        super().__init__(start=0, end=0, decay=0)
