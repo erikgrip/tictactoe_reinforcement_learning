5a23ec1c13f17cf69dce18fcfb6ba98cd130b5eb
Erik <erik_grip@hotmail.com>
2020-09-20 15:42:28 +0200

    Rename Base -> BaseStrategy and add unimplmented select_action method.

--- tic_tac_toe/agent/strategy/base_strategy.py
+++ tic_tac_toe/agent/strategy/base_strategy.py
@@ -1,7 +1,7 @@
 ''' Strategies are balancing exploitation and exploration. '''
 import math
 
-class BaseStratregy():
+class BaseStrategy():
     def __init__(self, start, end, decay):
         # Basic input validation
         if (start < 0) | (end < 0) | (decay < 0) :
@@ -12,8 +12,11 @@ class BaseStratregy():
         self.start = start
         self.end = end
         self.decay = decay
-        self.current_step = 0 
-        
+        self.current_step = 0
+
+    def select_action(self, aq_pairs):
+        raise NotImplementedError
+
     def get_decayed_rate(self):
         return self.end + (self.start - self.end) * \
             math.exp(-1. * self.current_step * self.decay)
