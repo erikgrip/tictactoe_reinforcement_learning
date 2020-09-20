6913a958d3549e2aa7fda382a4f89859cd67e275
Erik <erik_grip@hotmail.com>
2020-09-20 15:44:30 +0200

    Minor changes

--- tic_tac_toe/agent/agent.py
+++ tic_tac_toe/agent/agent.py
@@ -7,11 +7,11 @@ class Agent:
 
     def get_current_epsilon(self):
         return self.strategy.get_exploration_rate(self.current_step)
-    
+
     def get_action(self, state, valid_actions):
         qs = self.model.get_policy_qs(state)[0]
-        qs = list(enumerate(qs))  # Assuming 0 based action space
+        aqs = list(enumerate(qs))  # Assuming 0 based action space
         # Pass only valid actions, with their q values (a, q)
-        valid_qs = np.array([a_q for a_q in qs if a_q[0] in valid_actions])
+        valid_qs = np.array([a_q for a_q in aqs if a_q[0] in valid_actions])
         action = self.strategy.select_action(valid_qs)
         return action
