904afcd8b225d627134e142a84e4e9ef339dd6ae
egrip <erik_grip@hotmail.com>
2022-01-05 23:22:50 +0100

    Refactor DQN

--- tic_tac_toe/agent/algorithm/dqn.py
+++ tic_tac_toe/agent/algorithm/dqn.py
@@ -1,6 +1,3 @@
-from keras.models import Sequential
-from keras.layers import Activation, Conv2D, Dense, Flatten, Dropout
-from keras.optimizers import Adam
 import numpy as np
 
 class DQN():
@@ -10,57 +7,62 @@ class DQN():
         self.update_target_weights()
         self.discount = discount
 
+
     def _get_qs(self, model, states) -> np.array:
         return model.predict(
             # Use [-3:] not to predict on batch size dimension
             np.array(states).reshape(-1,*states.shape[-3:]))
 
+
     def get_policy_qs(self, states) -> np.array:
         return self._get_qs(self.policy_model, states)
 
+
+    def _valid_max_q(self, qs, valid_moves) -> float:
+        mask = np.zeros(qs.shape[0], dtype=int)
+        mask[valid_moves] = 1
+        return np.max(qs[mask == True])
+
+
+    def _prep_training_input(self, experiences):
+        current_states = np.array([exp[0] for exp in experiences])
+        future_states = np.array([exp[3] for exp in experiences])
+        current_qs_list = self._get_qs(self.policy_model, current_states)
+        future_qs_list = self._get_qs(self.target_model, future_states)
+
+        X = []
+        y = []
+        for index, (state, action, reward, _, next_valid_moves,
+                    is_terminal_state) in enumerate(experiences):
+
+            if not is_terminal_state:
+                future_qs = future_qs_list[index]
+                # Only consider valid moves for next state
+                max_future_q = self._valid_max_q(future_qs, next_valid_moves)
+                new_q = reward + self.discount * max_future_q
+            else:
+                new_q = reward
+
+            current_qs = current_qs_list[index]
+            current_qs[action] = new_q
+
+            X.append(state)
+            y.append(current_qs)
+        return X, y
+
+
     def train(self, experiences, game_done, callbacks=None):
-            batch_size = len(experiences)
-            # Extract states, actions, rewards and next_states into
-            #their own tensors from a given Experience batch
-            current_states = np.array(
-                [experience[0] for experience in experiences])
-            current_qs_list = self.get_policy_qs(current_states)
-            new_states = np.array(
-                [experience[3] for experience in experiences])
-            future_qs_list = self.get_target_qs(new_states)
+        X, y = self._prep_training_input(experiences)
 
-            X = []
-            y = []
+        # Fit on all samples as one batch, log only on terminal state
+        self.policy_model.model.fit(
+            np.array(X),
+            np.array(y),
+            batch_size=len(experiences),
+            verbose=0,
+            shuffle=False,
+            callbacks=callbacks if game_done else None)
 
-            # The long tuple is the transistions in the minibatch
-            for index, (
-                    state, action, reward, next_state,
-                    next_valid, terminal_state) in enumerate(experiences):
-
-                if not terminal_state:
-                    future_qs = future_qs_list[index]
-                    # Only consider valid moves for next state
-                    mask = np.zeros(future_qs.shape[0], dtype=int)
-                    mask[next_valid] = 1
-                    max_future_q = np.max(future_qs[mask == True])
-                    new_q = reward + self.discount * max_future_q
-                else:
-                    new_q = reward
-
-                current_qs = current_qs_list[index]
-                current_qs[action] = new_q
-
-                X.append(state)
-                y.append(current_qs)
-
-            # Fit on all samples as one batch, log only on terminal state
-            self.policy_model.model.fit(
-                np.array(X),
-                np.array(y),
-                batch_size=batch_size,
-                verbose=0,
-                shuffle=False,
-                callbacks=callbacks if game_done else None)
 
     def update_target_weights(self):
         self.target_model.model.set_weights(
