d8a617bc9ed5e42ce14705437e3a0976eceee947
Erik Grip <erik.grip@outlook.com>
2022-01-12 21:28:40 +0100

    Update agent strategy access with property getter and setter

--- tic_tac_toe/agent/agent.py
+++ tic_tac_toe/agent/agent.py
@@ -2,11 +2,21 @@ import numpy as np
 
 class Agent:
     def __init__(self, strategy, model):
-        self.strategy = strategy
-        self.model = model
+        self._strategy = strategy
+        self._model = model
+
+
+    @property
+    def strategy(self):
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, s):
+        self._strategy = s
+
 
     def get_action(self, state, valid_actions):
-        qs = self.model.get_policy_qs(state)[0]
+        qs = self._model.get_policy_qs(state)[0]
         aqs = list(enumerate(qs))  # Assuming 0 based action space
         # Pass only valid actions, with their q values (a, q)
         valid_qs = np.array([a_q for a_q in aqs if a_q[0] in valid_actions])
